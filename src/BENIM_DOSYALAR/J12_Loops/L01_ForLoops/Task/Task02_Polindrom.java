695aa4b9c09e4f48006b411b5ce0e1069fabb400
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-06 01:26:25 +0300

    06.10.22morning

--- src/BENIM_DOSYALAR/J12_Loops/L01_ForLoops/Task/Task02_Polindrom.java
+++ src/BENIM_DOSYALAR/J12_Loops/L01_ForLoops/Task/Task02_Polindrom.java
@@ -7,30 +7,46 @@ public class Task02_Polindrom {
    /*
         girilen ifadenin polindrome olmasını kontrol eden method create ediniz...
         plindrome:her iki yönde okundugunda eşit olan ifadeler
-​
+
          */
 
         Scanner sc = new Scanner(System.in);
         System.out.print("bir metin  giriniz : ");
         String str = sc.nextLine();
 
-        polindrome(str);//String parametreli method call
+        polindrome1(str);//String parametreli method call
 
 
     }//main dışı
 
-    private static void polindrome(String str) {//
-        String  tersStr="";//bos bir String kutu kulah
-        for (int i =str.length()-1; i >=0 ; i--) {
+    private static void polindrome1(String str) {
+        String rvrstr="";
+        for (int i =str.length()-1 ; i >=0 ;i--){
 
-            tersStr+=str.charAt(i);//strnin i.Ci karakterini tersStr'ye ekler(concat)
+            rvrstr+=str.charAt(i);
         }
-        System.out.println("girilen ifadenin tersi :"+tersStr);
+        System.out.println( "ifadenin tersi = " +rvrstr);
+       if (str.equalsIgnoreCase(rvrstr)) {
+           System.out.println("Girilen ifade Polindrome");
+       }else {
+           System.out.println("Girilen ifade Polindrome degildir");
+       }
 
-        if (str.equalsIgnoreCase(tersStr)){//str ile tersSTr eşitlik kontrolu
-            System.out.println("girilen ifade POLİNDROME ");
 
-        }else System.out.println("girlen ifade polindrome değil");
+    }
+
+    // private static void polindrome(String str) {//
+  //     String  tersStr="";//bos bir String kutu kulah
+  //     for (int i =str.length()-1; i >=0 ; i--) {
+
+  //         tersStr+=str.charAt(i);//strnin i.Ci karakterini tersStr'ye ekler(concat)
+  //     }
+  //     System.out.println("girilen ifadenin tersi :"+tersStr);
+
+  //     if (str.equalsIgnoreCase(tersStr)){//str ile tersSTr eşitlik kontrolu
+  //         System.out.println("girilen ifade POLİNDROME ");
+
+  //     }else System.out.println("girlen ifade polindrome değil");
 
 
 
@@ -46,4 +62,4 @@ public class Task02_Polindrom {
 
 
 
-}
+
--- src/BENIM_DOSYALAR/J12_Loops/L01_ForLoops/Task/Task03_Faktoriyel.java
+++ src/BENIM_DOSYALAR/J12_Loops/L01_ForLoops/Task/Task03_Faktoriyel.java
@@ -5,31 +5,63 @@ import java.util.Scanner;
 public class Task03_Faktoriyel {
     public static void main(String[] args) {
 
-         /*
+
+
+        /*
 Task -> girilen sayının faktoriyelini hesaplayan method create ediniz
 sayi!= 1*2*3* ...*sayi
   */
-        Scanner sc= new Scanner(System.in);
-        System.out.println("bir pozitif tam sayı giriniz : ");
-        int sayı=sc.nextInt();
+  //     Scanner sc= new Scanner(System.in);
+  //     System.out.println("bir pozitif tam sayı giriniz : ");
+  //     int sayı=sc.nextInt();
 
-        System.out.println(faktoriyel(sayı));//int parametreli method call
-    }//main dışı
+  //     System.out.println(faktoriyel(sayı));//int parametreli method call
+  // }//main dışı
 
-    private static int faktoriyel(int sayı) {
-        int faktoriyel = 1;
+  // private static int faktoriyel(int sayı) {
+  //     int faktoriyel = 1;
 
-        for (int i = 1; i <= sayı; i++) {
-            faktoriyel *= i;
+  //     for (int i = 1; i <= sayı; i++) {
+  //         faktoriyel *= i;
+
+  //     }
+  //     // System.out.println("faktoriyel : "+faktoriyel);
+  //     return faktoriyel;
+
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Lutfeen bir sayi giriniz");
+        int sayi = scan.nextInt();
+       // faktoriyel1(sayi);
+        System.out.println(faktoriyel2(sayi));
+
+    }
+
+    private static int faktoriyel2(int sayi) {
+        int faktoriyel =1;
+        for (int i=1; i<=sayi;i++){
+            faktoriyel*=i;
 
         }
-        // System.out.println("faktoriyel : "+faktoriyel);
-        return faktoriyel;
 
+        //System.out.println("faktoriyel  = " +faktoriyel);// sonsuz donguye girer
+
+        return faktoriyel;
 
 
     }
 
+    public static void faktoriyel1(int sayi){
+        int faktoriyel =1;
+
+        for (int i=1 ;i <=sayi ;i++){
+
+             faktoriyel *= i;
+        }
+    System.out.println("faktoriyel  = " +faktoriyel);
+
+
+
+}
 
 
 
