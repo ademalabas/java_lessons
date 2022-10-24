892cb1819a49c67a487f0112120507e0240c76e9
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-24 09:31:28 +0300

    24.10.22

---
+++ src/BENIM_DOSYALAR/J19_StaticVariable/Task02.java
@@ -0,0 +1,18 @@
+package BENIM_DOSYALAR.J19_StaticVariable;
+
+public class Task02 {
+/*
+    Task 02 ->
+    ElektirikHesap Class:  fields : toplamTuketim(int), oran(double), fatura(double)
+    Müşteriye ait tüketilen enerjiyi toplamTuketim'e toplayan tüketimEkle method
+    oran(0.7)  katsaysı ile  toplamTuketim çarparak fatura değeri atayan odenecekTutar method
+
+    Müsteri Class: fields: name ElektrikHesabi class obj.
+    Runner Class obj ile enz 2  aylık elektrik tüketim faturası print eden code create ediniz
+
+     */
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/BENIM_DOSYALAR/J19_StaticVariable/Task03.java
@@ -0,0 +1,17 @@
+package BENIM_DOSYALAR.J19_StaticVariable;
+
+public class Task03 {
+ /*
+    Task 03 ->
+    Lesson Class :  fields : name, credit (1-10 arasında değer almalı)
+    Student Class : fields : name, maxCredit, dersleri(Lesson class obj) saklayacak list
+                   derslerin toplam kredi miktarını return eden  toplamKredisi method
+    Runner Class :  3 adet  ders ve 1 adet öğrc (maxCredit=10) obj create ederek öğrencinin alabileceği ve
+                       alamayacagı dersleri print eden code create ediniz
+
+     */
+
+    public static void main(String[] args) {
+        
+    }
+}
