fa566a61903662c86f805ce813058d65493c996d
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-27 00:04:16 +0300

    27.10.22

--- src/BENIM_DOSYALAR/J22_DateTime/QuestionOfTheWeek/Kullanici.java
+++ src/BENIM_DOSYALAR/J22_DateTime/QuestionOfTheWeek/Kullanici.java
@@ -3,14 +3,11 @@ package BENIM_DOSYALAR.J22_DateTime.QuestionOfTheWeek;
 import java.time.LocalDateTime;
 
 public class Kullanici {
-
     String name;
     LocalDateTime kayitZamani;
 
-
-    public Kullanici(String name, LocalDateTime kayitZamani){
-
+    public Kullanici(String name, LocalDateTime kayitZamani) {
+        this.name = name;
+        this.kayitZamani = kayitZamani;
     }
-
-
 }
