d5a3615b9b2bf1026bcc8a5c85019111b7010a3e
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-09 00:11:53 +0300

    09.11.22

---
+++ src/BENIM_DOSYALAR/j29_Exceptions/C02_FileInputStreamException02.java
@@ -0,0 +1,35 @@
+package BENIM_DOSYALAR.j29_Exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C02_FileInputStreamException02 {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("/Users/yeni/IdeaProjects/javaLearn/src/j29_Exceptions/ebikGabı");
+            int k;
+
+            try {
+                while ((k = fis.read()) != -1) {
+                    System.out.print((char) k);
+                }
+                System.out.println("agam içteki  try block'tan selamlar :) ");
+            } catch (IOException e) {//dosyaya ulaşılır fakat okunamzsa bu catch çalışır
+                System.out.println("agam  dosya okuanamayırrr ....");
+                System.out.println("agam içteki  catch  block'tan selamlar :) ");
+            }
+            System.out.println("agam dışdaki  try block'tan selamlar :) ");
+
+        } catch//dosyaya ulaşılmazza bu catch çalışır
+        (FileNotFoundException e) {
+            System.out.println("agam  dosya ulaşılamıyor  ....");
+            System.out.println("agam dışdaki  catch  block'tan selamlar :) ");
+        }
+        System.out.println("agam sorun handle edildi DEWAMKEEE :)");
+    }
+
+
+
+
+}
