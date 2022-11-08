e9af4e5171e04fe70b4f995e0f7fed83b86ce125
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-09 01:53:55 +0300

    09.11.22

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,9 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         StringBuilder strB1 = new StringBuilder("AVATAR");
-
         strB1.subSequence(1,5);
-
         strB1.reverse();
-
         strB1.deleteCharAt(2);
-
         System.out.println(strB1);
 
 
