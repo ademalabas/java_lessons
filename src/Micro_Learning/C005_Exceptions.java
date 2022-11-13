ef595e6bb1a49cb32a4dd51aa243a55cf2afb139
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-13 13:30:06 +0300

    13.11.22

--- src/Micro_Learning/C005_Exceptions.java
+++ src/Micro_Learning/C005_Exceptions.java
@@ -6,7 +6,16 @@ public class C005_Exceptions {
     public static void main(String[] args) {
         ArrayList myList = new ArrayList();
         String [] myArray;
-
+        try {
+            while (true) {
+                myList.add("My String");
+            }
+        }catch (RuntimeException re){
+            System.out.println("Caught a RuntimeException");
+        }catch (Exception e){
+            System.out.println("Caught an Exception");
+        }
+        System.out.println("Ready to use :");
 
 
 
