80d1489cf1ab843c3db8a3ec0d6317e055106dd6
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-18 21:51:37 +0300

    18.11.22

--- src/BENIM_DOSYALAR/J35_Collection/C02_Set/C01_HashSet.java
+++ src/BENIM_DOSYALAR/J35_Collection/C02_Set/C01_HashSet.java
@@ -49,13 +49,46 @@ public class C01_HashSet {
     public static void main(String[] args) {
 
         HashSet <String>    hs =new HashSet<>();// boş bir list
-        HashSet <String>    hs1 =new HashSet<>(Arrays.asList("Ercan, Yakup, Adem, Rukiye, Nur"));// eleman atamasi yapilan bir hasset
+        HashSet <String>    hs1 =new HashSet<>(Arrays.asList("Ercan", "Yakup", "Adem", "Rukiye", "Nur"));// eleman atamasi yapilan bir hasset
         // HashSet  print etme
-        System.out.println("hs1 = " + hs1);//[Ercan, Yakup, Adem, Rukiye, Nur]
+        System.out.println("hs1 = " + hs1);//Ercan, Adem, Kevser, Nur, Yakup, Rukiye]
         hs1.add("Kevser");//
-        System.out.println(hs1);//[Kevser, Ercan, Yakup, Adem, Rukiye, Nur]
+        System.out.println(hs1);//Ercan, Adem, Kevser, Nur, Yakup, Rukiye]
         hs1.add("Adem");//
-        System.out.println("hs1 = " + hs1);//[Adem, Kevser, Ercan, Yakup, Adem, Rukiye, Nur]
+        System.out.println("hs1 = " + hs1);//Ercan, Adem, Kevser, Nur, Yakup, Rukiye]
+        hs1.add(null);
+        hs1.add(null);
+        hs1.add(null);
+        System.out.println("hs1 = " + hs1);//null Ercan, Adem, Kevser, Nur, Yakup, Rukiye]
+
+        HashSet<String > hs2=new HashSet<>(Arrays.asList("Basarı","Gayrete ","Asıktır"));
+        System.out.println("hs2 = " + hs2);//[Basarı, Asıktır, Gayrete ]
+        hs1.addAll(hs2);
+        System.out.println("hs1 = " + hs1);//[null, Basarı, Ercan, Adem, Kevser, Asıktır, Nur, Yakup, Rukiye, Gayrete ]
+
+        //set remove()-> ğistenene eleman set'ten silinir
+        System.out.println("hs1.remove(\"asıktır\") = " + hs1.remove("asıktır"));//omayan eleman silinirse->false
+        System.out.println("hs1.remove(\"Asıktır\") = " + hs1.remove("Asıktır"));//varolan eleman silinirse ->true
+        System.out.println("hs1 = " + hs1);//[null, Basarı, Ercan, Adem, Kevser, Nur, Yakup, Rukiye, Gayrete ]
+
+        System.out.println("hs1.removeAll(hs2) = " + hs1.removeAll(hs2));//true
+        System.out.println("hs1 = " + hs1);//[null, Ercan, Adem, Kevser, Nur, Yakup, Rukiye]
+
+        System.out.println("hs1.size() = " + hs1.size());// 7
+
+        System.out.println("hs1.isEmpty() = " + hs1.isEmpty());// false
+
+        hs1.clear();
+
+        System.out.println("hs1.isEmpty() = " + hs1.isEmpty());// true
+
+        System.out.println("hs1.size() = " + hs1.size());//0
+        System.out.println("hs1 = " + hs1);// []
+
+        System.out.println("hs2.contains(\"Basarı\") = " + hs2.contains("Basarı"));// true
+
+        System.out.println("hs2.contains(\"javaNAZZZ\") = " + hs2.contains("javaNAZZZ"));// false
+        System.out.println("hs2.hashCode() = " + hs2.hashCode());//1769718834
 
 
     }
--- src/BENIM_DOSYALAR/J35_Collection/C02_Set/C02_Task.java
+++ src/BENIM_DOSYALAR/J35_Collection/C02_Set/C02_Task.java
@@ -1,7 +1,10 @@
 package BENIM_DOSYALAR.J35_Collection.C02_Set;
 
-public class C02_HashSet {
+public class C02_Task {
     public static void main(String[] args) {
+
+
+
 
     }
 }
