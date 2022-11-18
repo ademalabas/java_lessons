0fb1cb550d398cefa09efb139e29661cafdb8872
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-18 21:29:44 +0300

    18.11.22

---
+++ src/BENIM_DOSYALAR/J35_Collection/C02_Set/C01_HashSet.java
@@ -0,0 +1,62 @@
+package BENIM_DOSYALAR.J35_Collection.C02_Set;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class C01_HashSet {
+
+    /*
+     Set :  1) unique(benzersiz tek) elemanlardan olusur, Set'lerin icinde bir eleman sadece bir kere kullanilabilir. mükerrer(duplicate) eleman olmaz.
+            2) sadece bir null değeri alabilir.(treeSet hariç).Java elementleri unique yapmak icin HASH ALGORITMASI kullanir.
+                 Java her data icin hashing teknigini kullanarak bir code uretir.Bu code'u almak icin HashCode() methodu kullanilir.
+                 Hashing, farklı büyüklükteki girdilerden sabit büyüklükte bir çıktı oluşturma sürecine verilen isimdir.
+                  Universitelerdeki ogrenci numaralari gibi bir ogrenci ismi soruldugunda numarasini bulursaniz onunla ilgili tum datalara ulasilabilir.
+
+
+            Set     --> HashSet         : 1) HashSet, duplication’a(tekrarlı eleman) izin vermez.
+                                            Eger bir elemani tekrar HashSet’e eklemek isterseniz eski olan silinip, yeni olan uzerine yazilir.
+                                          2) Haset'ler Set'ler arasinda en hizli olanıdir. bir algoritmaya göre kayit eder.
+                                           değerlerin giriş sırasına dikkat edilmez. (LinkedHashSet hariç)
+                                          3) HashSet "null" object'ini eleman olarak kullanabilir.
+                                          Ancak birden fazla null degerini bir HashSet’e eklemek isterseniz sadece bir tane null degeri olur.
+
+                        LinkedHashset   : 1) Tekrarli eleman kabul etmezler. Elemanlari ekleme sirasina(insertion order) gore dizerler.
+                                          2) LinkedHashSet, HashSet'den daha yavas calisir.
+                                          3)LinkedHashSet "null" object'ini eleman olarak kullanabilir.
+                                          4) Ekleme ve remove islemlerinde hizlidirlar.
+
+                        TreeSet         : 1) TreeSet tekrarli eleman kabul etmez, cunku Set'dir.
+		                                  2) Elemanlari natural Order'a(String ise alfabetik, sayi ise kucukten buyuge) gore dizer.
+		 	                              3) TreeSet, setlerin en yavasidir. Bu yuzden TreeSet kullanirken dikkatli olunmalı...
+		 	                              4)AHAN DA TRİCK :TreeSet "null" object'ini eleman olarak kullanilamaz.
+
+
+         Set Methods :
+            .add(eleman);-->Set’e eleman ekler
+            .addAll(collection);-->istenen collection’in tum elemanlarini ekler
+            .contains(eleman);-->istenen eleman set’te varsa true, yoksa false return eder.
+            .containsAll(collection);-->istenen collection’in tumu aranan sette var ise true, yoksa false return eder.
+            .remove(eleman);-->istenen eleman bulursa siler ve true dondurur, bulamazsa false return eder.
+            .removeAll(collection);-->istenen collection’in tum elemanlarini bulursa siler ve true dondurur, bulamazsa false return eder.
+            .equlas(set2);-->istenen set2’le tum elemanlar ayni ise true, yoksa false return eder.
+            .retainAll(collection1);-->collection1’nin elemanlarinin disindaki tum elemanlari siler, silme islemi yapti ise true, yoksa false return eder.
+              (kesişen ortak elemanları döndürür.)
+            .clear();-->sett'teki Tum elemanlari siler
+            .isEmpty();-->Sette hic eleman yoksa true, varsa false return eder
+            .size();-->set’in eleman sayisini verir
+
+     */
+    public static void main(String[] args) {
+
+        HashSet <String>    hs =new HashSet<>();// boş bir list
+        HashSet <String>    hs1 =new HashSet<>(Arrays.asList("Ercan, Yakup, Adem, Rukiye, Nur"));// eleman atamasi yapilan bir hasset
+        // HashSet  print etme
+        System.out.println("hs1 = " + hs1);//[Ercan, Yakup, Adem, Rukiye, Nur]
+        hs1.add("Kevser");//
+        System.out.println(hs1);//[Kevser, Ercan, Yakup, Adem, Rukiye, Nur]
+        hs1.add("Adem");//
+        System.out.println("hs1 = " + hs1);//[Adem, Kevser, Ercan, Yakup, Adem, Rukiye, Nur]
+
+
+    }
+}
---
+++ src/BENIM_DOSYALAR/J35_Collection/C02_Set/C02_HashSet.java
@@ -0,0 +1,7 @@
+package BENIM_DOSYALAR.J35_Collection.C02_Set;
+
+public class C02_HashSet {
+    public static void main(String[] args) {
+
+    }
+}
