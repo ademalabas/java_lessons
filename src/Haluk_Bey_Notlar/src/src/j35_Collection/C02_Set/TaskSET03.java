c6aa326d42a7a4299af1661fc7313339bc73d8e0
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-19 01:40:08 +0300

    19.11.22

---
+++ src/Haluk_Bey_Notlar/src/src/j35_Collection/C02_Set/TaskSET03.java
@@ -0,0 +1,22 @@
+package Haluk_Bey_Notlar.src.src.j35_Collection.C02_Set;
+
+public class TaskSET03 {
+
+    /*
+    Task
+    totalCount() isminde bir method oluşturun.
+    Parametresi  'Integer Hashset' olmalı
+
+    HashSetteki eleman sayısını alın.
+    totalCount'u döndürün.
+    Örnek:
+    hashset ; 4,2,3,1,7
+    cevap: 5
+     */
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
