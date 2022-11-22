cc3b29cc726c835c534f1b548e1d3b615b7f2c51
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-22 21:04:25 +0300

    22.11.22

--- src/BENIM_DOSYALAR/J36_Map/C02_KeySet_values.java
+++ src/BENIM_DOSYALAR/J36_Map/C02_KeySet_values.java
@@ -2,7 +2,7 @@ package BENIM_DOSYALAR.J36_Map;
 
 import java.util.HashMap;
 
-public class C01_HashMap {
+public class C02_KeySet_values {
     public static void main(String[] args) {
 /*  Map bir interface'dir. Map interface'inin 3 tane child class'i vardir
 
@@ -14,20 +14,31 @@ public class C01_HashMap {
         HashMap'ler tanımlanırken rastgele siralama yapar  HasMap map'ler arasinda en hizlisidir.
         HashMap'ler "thread safe" degildir.
                 */
-
         HashMap<String, String> hm = new HashMap<>();
-        //map.put();->girilen key, value datayı  map'e eleman olarak ekler.
+        //map.keySet();->map key değerlerini Set'e atayıp return eder.
+        //map.values();->map value değerlerini coll. atayıp return eder.
         hm.put("Amazon", "296 Euro");
-        hm.put(null, "200 Euro");//key null değer kabul eder.
-        hm.put(null, "222 Euro");//tekrarlı key değerine value update edilir. Aynı key kullanılırda uzerine atama yapılır
-        hm.put("Ebay", null);//value dağeri null atanabilir
+        hm.put("Ebay", "234 Euro");
         hm.put("Saturn", "300 Euro");
         hm.put("Vatan", "111 Euro");
         hm.put("Apple store", "333 Euro");
-        hm.put("Apple store", "500 Euro");//Apple store key'deki value 333 Euro iken 500 Euro olarak update etti.
         hm.put("MediaMarkt", "183 Euro");
-        //map sout komutuna parametre olarak print edilir...
-        System.out.println("hm = " + hm);//{null=222 Euro, Saturn=300 Euro, Ebay=null, Apple store=500 Euro, MediaMarkt=183 Euro, Amazon=296 Euro, Vatan=111 Euro}
+
+        System.out.println("hm = " + hm);
+
+        System.out.println("hm.keySet() = " + hm.keySet());//[Saturn, Ebay, Apple store, MediaMarkt, Amazon, Vatan]
+        System.out.println("hm.values() = " + hm.values());//[300 Euro, 234 Euro, 333 Euro, 183 Euro, 296 Euro, 111 Euro]
+        System.out.println("   ***   ");
+        //Task-> hm key ve value değerlerini satır satır print ediniz.with loop(for-each)
+
+
+        for (String s : hm.keySet()) {//key
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        for (String w : hm.values()) {//value
+            System.out.print(w + " ");
+        }
     }
 
 
