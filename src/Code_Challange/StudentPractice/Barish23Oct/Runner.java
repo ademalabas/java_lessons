7c2b26fb4f42f4026d1510a2a609bc7c54a849f4
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-23 23:34:18 +0300

    23.11.22

---
+++ src/Code_Challange/StudentPractice/Barish23Oct/Runner.java
@@ -0,0 +1,24 @@
+package Code_Challange.StudentPractice.Barish23Oct;
+
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+Student student = new Student();
+student.ogrenciKaydet();
+
+
+    }// main sonu
+
+
+
+
+
+
+
+}
