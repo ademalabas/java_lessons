06e0d341bf1445c5b7ba25e20be4ee04051f5075
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-26 23:46:24 +0300

    26.11.22

--- src/BENIM_DOSYALAR/J40_Btk/workingWirhFiles/C01_Files.java
+++ src/BENIM_DOSYALAR/J40_Btk/workingWirhFiles/C01_Files.java
@@ -2,12 +2,21 @@ package BENIM_DOSYALAR.J40_Btk.workingWirhFiles;
 
 
 import java.io.File;
+import java.io.IOException;
 
 public class C01_Files {
 
     public static void main(String[] args) {
         File file = new File("C:\\Users\\hi\\IdeaProjects\\java_lessons\\File\\student.txt");
-
+        try {
+            if(file.createNewFile()){
+                System.out.println("Dosya Olusturuldu");
+            }else {
+                System.out.println("Dosya zaten mevcut");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
