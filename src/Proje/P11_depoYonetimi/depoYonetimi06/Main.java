0557454fcbee4e92dcc45f4f077fe1cac6fa9006
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-26 20:29:21 +0300

    26.11.22

--- src/Proje/P11_depoYonetimi/depoYonetimi06/Main.java
+++ src/Proje/P11_depoYonetimi/depoYonetimi06/Main.java
@@ -3,8 +3,17 @@ package Proje.P11_depoYonetimi.depoYonetimi06;
 public class Main extends Method {
     public static void main(String[] args) {
 
+        UrunTanimlama urun1=new UrunTanimlama(1000,"Peynir","MuratBey","kg");
+        UrunTanimlama urun2=new UrunTanimlama(1001,"Çikolata","ülker","adet");
+        UrunTanimlama urun3=new UrunTanimlama(1002,"Salça","Olca","kg");
+        UrunTanimlama urun4=new UrunTanimlama(1003,"Şeker","Balküpü","çuval");
 
         Main depoCalis = new Main();
+        urunler.put(urun1.getId(),urun1);
+
+        urunler.put(urun2.getId(),urun2);
+        urunler.put(urun3.getId(),urun3);
+        urunler.put(urun4.getId(),urun4);
         depoCalis.girisPaneli();
     }
 }
--- src/Proje/P11_depoYonetimi/depoYonetimi06/Method.java
+++ src/Proje/P11_depoYonetimi/depoYonetimi06/Method.java
@@ -8,13 +8,16 @@ public class Method implements GirisCikisInterface {
     public static final String G = "\u001B[32m";
     public static final String Y = "\u001B[33m";
     public static final String B = "\u001B[34m";
+    public static final String P = "\u001B[35m";
     static Scanner scan = new Scanner(System.in);
-    static int id=1000;
-    static HashMap<Integer,UrunTanimlama> urunler=new HashMap<>();
-    static int count=0;
-    public void girisPaneli(){
+    static int id = 1004;//***
+    static HashMap<Integer, UrunTanimlama> urunler = new HashMap<>();
 
-        System.out.println(Y+ "========================== İŞLEMLER =======================\r\n"
+    static int count = 0;
+
+    public void girisPaneli() {
+
+        System.out.println(Y + "========================== İŞLEMLER =======================\r\n"
                 + "   ____________________              ____________________   \n"
                 + "   | 1-URUN TANIMLAMA |              |  2-URUN LİSTELE  |   \n"
                 + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   \n"
@@ -23,16 +26,17 @@ public class Method implements GirisCikisInterface {
                 + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   \n"
                 + "   ____________________              ____________________   \n"
                 + "   | 5-URUN CIKISI    |              |  6-BITIRME       |   \n"
-                + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  "+R);
+                + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  " + R);
 
         System.out.print("Yapmak istediginiz islem : ");
 
         try {
-            int secim= scan.nextInt();
+            int secim = scan.nextInt();
 
-            switch (secim){
+            switch (secim) {
                 case 1:
                     urunTanimlama();
+
                     girisPaneli();
                     break;
                 case 2:
@@ -41,17 +45,18 @@ public class Method implements GirisCikisInterface {
                     break;
                 case 3:
                     miktarGuncelle();
-                    urunListele();
+
+                    //urunListele(); ***
                     girisPaneli();
                     break;
                 case 4:
                     rafaKoy();
-                    urunListele();
+                    //urunListele();***
                     girisPaneli();
                     break;
                 case 5:
                     urunCikis();
-                    urunListele();
+                    //urunListele();***
                     girisPaneli();
                     break;
                 case 6:
@@ -62,90 +67,191 @@ public class Method implements GirisCikisInterface {
                     girisPaneli();
                     break;
             }
-        }catch (InputMismatchException e) {
+        } catch (InputMismatchException e) {
             System.out.println("Sayi disinda karakter girilemez");
             scan.nextLine();
             girisPaneli();
-        }}
+        }
+    }
 
     public void cikisYap() {
         System.out.println("Cikis yapildi");
+        System.exit(0);//***
+
     }
 
-    private static void urunTanimlama() {
+    public static void urunTanimlama() {
+
 
         System.out.println("lutfen urun bilgilerini giriniz");
         System.out.println("Urun ismi");
         scan.nextLine();
-        String urunIsmi=scan.nextLine();
+        String urunIsmi = scan.nextLine();
+        Set<Map.Entry<Integer, UrunTanimlama>> urunlerSeti = urunler.entrySet();
+
+        try {//*** Aynı ürün isim kontrolu için for döngüsü ile karşılaştırdık..
+            for (Map.Entry<Integer, UrunTanimlama> each : urunlerSeti) {
+
+                if (each.getValue().getUrunIsmi().equals(urunIsmi)) {
+                    System.out.println(P + "Ürün mevcut Lütfen tekrar ürün girişi yapmak için (D)' ye basın ");
+                    scan.next();
+                    urunIsmi = "";
+                    urunTanimlama();
+                }
+            }
+        } catch (Exception e) {
+            Main g = new Main();
+            g.girisPaneli();
+        }
         System.out.println("Uretici firma");
-        String uretici=scan.nextLine();
+        String uretici = scan.nextLine();
         System.out.println("Birim");
-        String birim=scan.nextLine();
-        UrunTanimlama urun=new UrunTanimlama(id,urunIsmi,uretici,birim);
-        urunler.put(id,urun);
+        String birim = scan.nextLine();
+
+        UrunTanimlama urun = new UrunTanimlama(id, urunIsmi, uretici, birim);
+        urunler.put(id, urun);
+
+        System.out.println(P + "Ürün ID :" + urunler.get(id).getId() + "/  Adı :" + urunler.get(id).getUrunIsmi() + "/   Üretici : " + urunler.get(id).getUretici() + "/   Birimi : " + urunler.get(id).getBirim());//***
         id++;
 
     }
 
     public static void urunListele() {
-        Set<Map.Entry<Integer,UrunTanimlama>> urunlerSeti = urunler.entrySet();
+        Set<Map.Entry<Integer, UrunTanimlama>> urunlerSeti = urunler.entrySet();
 
         System.out.println("id       ismi         ureticisi       miktari       birimi         raf" +
                 "\n----------------------------------------------------------------------");
-        for (Map.Entry<Integer, UrunTanimlama> each: urunlerSeti) {
-            Integer urunlerKey= each.getKey();
-            System.out.printf( "%d    %-8s       %-14s %3d          %-14s %s"
-                    ,urunlerKey,each.getValue().getUrunIsmi(), each.getValue().getUretici(),each.getValue().getMiktar(),each.getValue().getBirim(), each.getValue().getRaf());
+        for (Map.Entry<Integer, UrunTanimlama> each : urunlerSeti) {
+            Integer urunlerKey = each.getKey();
+            System.out.printf("%d    %-8s       %-14s %3d          %-14s %s"
+                    , urunlerKey, each.getValue().getUrunIsmi(), each.getValue().getUretici(), each.getValue().getMiktar(), each.getValue().getBirim(), each.getValue().getRaf());
             System.out.println("");
         }
 
     }
+
     public static void miktarGuncelle() {
         System.out.println("Urun girisi icin Id bilgisi giriniz");
 
-        int arananId=scan.nextInt();
+        int arananId = scan.nextInt();
 
-        if(Character.isDigit(arananId)){
-            if(urunler.keySet().contains(arananId)) {
+        //if(!Character.isDigit(arananId)){//! işareti koyduk *** gereksiz olmuş ***
+        if (urunler.containsKey(arananId)) {
+            System.out.println(P + urunler.get(arananId).getUrunIsmi() + "  miktarı : " + urunler.get(arananId).getMiktar() + " Birim : " + urunler.get(arananId).getBirim());//***
 
-                System.out.println("Guncel miktarinizi giriniz");
-                int guncelMik = scan.nextInt();
-                urunler.get(arananId).setMiktar(guncelMik + urunler.get(arananId).getMiktar());
 
-            }else{
-                System.out.println("aradiniz urun yok");
-            }
-        }}
+            System.out.println("Depoya eklemek istedğiniz miktari giriniz");
+            int guncelMik = scan.nextInt();
+            urunler.get(arananId).setMiktar(guncelMik + urunler.get(arananId).getMiktar());
+
+
+            System.out.println(P + "Mevcut " + urunler.get(arananId).getUrunIsmi() + " miktarı  = " + urunler.get(arananId).getMiktar() + " " + urunler.get(arananId).getBirim());
+//*** yukarıdaki sout kullanıcıyı bilgilendirme amaçlı
+
+        } else {
+            System.out.println("aradiniz urun yok");
+        }
+    }
 
 
     private static void rafaKoy() {
         System.out.println("Raf girisi icin Id bilgisi giriniz");
-        int arananId= scan.nextInt();
+        int arananId = scan.nextInt();
 
-        if(urunler.keySet().contains(arananId)) {
+        if (urunler.keySet().contains(arananId)) {//*** menu
+            System.out.println(P + "Raflamak istedğiniz ürünün adı :" + urunler.get(arananId).getUrunIsmi() + " raf =" + urunler.get(arananId).getRaf());
+            System.out.println(P + "========================== İŞLEMLER =======================\r\n"
+                    + "   ____________________              ____________________   \n"
+                    + "   | 1-Şarküteri      |              | 2-Bakliyat       |   \n"
+                    + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   \n"
+                    + "   ____________________              ____________________   \n"
+                    + "   | 3-Atıştırmalık   |              | 4-Manav          |   \n"
+                    + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   \n"
+                    + "   ____________________              ____________________   \n"
+                    + "   | 5-İçecek         |              |  6-Temizlik      |   \n"
+                    + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  \n"
+                    + "   ____________________              ____________________   \n"
+                    + "   | 7-Züccaciye      |              |  8-Konserve      |   \n"
+                    + "   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  " + R);
 
-            System.out.println("Guncel raf giriniz");
+            System.out.print("Yapmak istediginiz islem : ");
+
+            //  System.out.println("Guncel raf giriniz \n1-Şarküteri\n2-Bakliyat \n3-Kasap \n4-Manav \n5-İçecek \n6-Temizlik \n7-Züccaciye \n Lütfen Seçiminizi giriniz :");
             scan.nextLine();
-            String guncelRaf = scan.nextLine();
-            urunler.get(arananId).setRaf(guncelRaf);
+            int rfgir = scan.nextInt();
+            try {
+                switch (rfgir) {//*** Bu switch case i koyduk try catch ile beraber
 
-        }else{
+                    case 1:
+                        String guncelRaf = "Şarküteri";
+                        urunler.get(arananId).setRaf(guncelRaf);
+
+                        break;
+                    case 2:
+                        guncelRaf = "Bakliyat";
+                        urunler.get(arananId).setRaf(guncelRaf);
+                        break;
+                    case 3:
+                        guncelRaf = "Atıştırmalık";
+                        urunler.get(arananId).setRaf(guncelRaf);
+                        break;
+
+                    case 4:
+                        guncelRaf = "Manav";
+                        urunler.get(arananId).setRaf(guncelRaf);
+                        break;
+                    case 5:
+                        guncelRaf = "İçecek";
+                        urunler.get(arananId).setRaf(guncelRaf);
+                        break;
+                    case 6:
+                        guncelRaf = "Temizlik";
+                        urunler.get(arananId).setRaf(guncelRaf);
+                        break;
+                    case 7:
+                        guncelRaf = "Züccaciye";
+                        urunler.get(arananId).setRaf(guncelRaf);
+                        break;
+                    case 8:
+                        guncelRaf = "Konserve";
+                        urunler.get(arananId).setRaf(guncelRaf);
+                        break;
+
+                    default:
+                        System.out.println("Böyle bir raf yoktur");
+                }
+                System.out.println(P + "Raflanan ürünün Adı :  " + urunler.get(arananId).getUrunIsmi() + " raf :" + urunler.get(arananId).getRaf());
+            } catch (Exception e) {
+                System.out.println("Lütfen Geçerli bir rafı sayı olarak giriniz ");
+                scan.nextLine();
+                rafaKoy();
+
+
+            }
+
+
+        } else {
             System.out.println("aradiniz urun yok");
         }
 
     }
+
     private static void urunCikis() {
         System.out.println("Urun cikisi icin Id bilgisi giriniz");
-        int arananId= scan.nextInt();
+        int arananId = scan.nextInt();
 
-        if(urunler.keySet().contains(arananId)) {
+        if (urunler.keySet().contains(arananId)) {
+            System.out.println(P + urunler.get(arananId).getUrunIsmi() + "  miktarı : " + urunler.get(arananId).getMiktar() + " Birim :" + urunler.get(arananId).getBirim());// ***
+            System.out.println("Çıkmak istediğiniz  miktari giriniz");//***
+            int cikisMik = scan.nextInt();
+            if (urunler.get(arananId).getMiktar() > 0 && (urunler.get(arananId).getMiktar() > cikisMik)) {//*** miktar olarak hiç yoksa çıkış yapılamaz uyarısı
 
-            System.out.println("Guncel miktarinizi giriniz");
-            int guncelMik = scan.nextInt();
-            urunler.get(arananId).setMiktar(urunler.get(arananId).getMiktar()-guncelMik);
 
-        }else{
+                urunler.get(arananId).setMiktar(urunler.get(arananId).getMiktar() - cikisMik);
+                System.out.println(P + "Mevcut " + urunler.get(arananId).getUrunIsmi() + "miktarı  = " + urunler.get(arananId).getMiktar() + " " + urunler.get(arananId).getBirim());//***
+                //***
+            } else System.out.println(P + "Ağam olmayan yada olandan fazla ürün çıkartmaya zorluyorsun");//***
+        } else {
             System.out.println("aradiniz urun yok");
         }
 
