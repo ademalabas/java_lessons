12af664e71c5cfe54190eb9d40b0f28ae2055fd2
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-26 23:36:34 +0300

    26.11.22

---
+++ src/BENIM_DOSYALAR/J40_Btk/workingWirhFiles/C01_Files.java
@@ -0,0 +1,16 @@
+package BENIM_DOSYALAR.J40_Btk.workingWirhFiles;
+
+
+import java.io.File;
+
+public class C01_Files {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\hi\\IdeaProjects\\java_lessons\\File\\student.txt");
+
+
+    }
+
+
+
+}
