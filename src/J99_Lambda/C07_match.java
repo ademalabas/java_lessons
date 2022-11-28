fae038cccba91e6fb8feeb6d30e155005e2a4008
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-28 20:43:05 +0300

    28.11.22

---
+++ src/J99_Lambda/C07_match.java
@@ -0,0 +1,19 @@
+package J99_Lambda;
+
+public class C07_match {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }// mainsonu
+//anyMatch() --> enaz bir eleman sarti saglarsa true aksi durumda false return eder
+//allMatch() --> tum  elemanlar sarti saglarsa true en az bir eleman sarti saglamazsa false return eder.
+//noneMatch()--> hic bir sarti SAGLAMAZSA true en az bir eleman sarti SAGLARSA false return eder.
+
+    // Task : List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz.
+
+
+}
