dbaeb851a9a86d2f09d8d97f97baf737a5af9c22
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-29 19:07:26 +0300

    29.11.22

--- src/J99_Lambda/C11_stream_iterator.java
+++ src/J99_Lambda/C11_stream_iterator.java
@@ -8,8 +8,27 @@ public class C11_stream_iterator {
 
 
         System.out.println("TASK01--> amele topla : "+toplaAmele(10));//55
+        System.out.println("   ***   ");
         System.out.println("TASK01--> cincix topla : "+toplaCincix(10));//55
+        System.out.println("   ***   ");
         System.out.println("TASK02--> cincix çift topla : "+toplaCift(24));//156
+        System.out.println("   ***   ");
+        System.out.println("TASK03--> cincix ilk 10 çift topla : " + toplaİlkXCiftSayi(10));//110
+        System.out.println("   ***   ");
+        System.out.println("TASK04--> cincix ilk 10 tek topla : " + toplaİlkXTekSayi(10));//100
+        System.out.println("   ***   ");
+        System.out.println("TASK05-->");
+        ikininİlkXKuvvetiPrint(7);//2 4 8 16 32 64 128
+        System.out.println("   ***   ");
+        System.out.println("   ***   ");
+
+
+
+
+
+
+
+
 
     }//main sonu
 
@@ -42,8 +61,39 @@ public class C11_stream_iterator {
                         sum();//akısa giren 1,2,3...x tam sayıları toplandı
         //rangeClosed(startInclusive,endInclusive);-> 1'lik artımlı bir adımla startInclusive'den (dahil) endInclusive'e (dahil) kadar  sıralı bir IntStream return eder.
     }
+    //TASK 03 --> Ilk x pozitif cift tam sayiyi toplayan code  create ediniz.
+    public static int toplaİlkXCiftSayi(int x) {
 
+        return IntStream.
+                iterate(2, t -> t + 2).//2'den başlayıp 2 artırılan tekrar işlemi tanımlandı->2,4,6,8.....
+                        limit(x).//akısdaki ilk x eleman akısa alınır
+                        sum();//akısdaki ilk x eleman toplandı
+        //iterate(seed, repeat action) --> seed'den başlayarak repeat action'a göre  sonsuza kadar elemanları akısa alır.
+    }
 
+    //TASK 04 --> Ilk X pozitif tek tamsayiyi toplayan programi  create ediniz.
+    public static int toplaİlkXTekSayi(int x) {
+
+        return IntStream.
+                iterate(1, t -> t + 2).//1'den başlayıp 2 artırılan tekrar işlemi tanımlandı->1,3,5...
+                        limit(x).//akısdaki ilk x eleman akısa alınır
+                        sum();//akısdaki ilk x eleman toplandı
+    }
+    //TASK 05 --> 2'nin ilk x kuvvetini ekrana yazdiran code  create ediniz.
+    public static void ikininİlkXKuvvetiPrint(int x){
+        IntStream.
+                iterate(2,t->t*2).//2'den başlayıp 2ile çarpılarak tekrar işlemi tanımlandı->2,4,8...
+                limit(x).
+                forEach(C01_LambdaExpression::yazdir);
+
+    }
+
+        //TASK 06 --> Istenilen bir sayinin ilk x kuvvetini ekrana yazdiran programi  create ediniz.
+
+        //TASK 07 --> Istenilen bir sayinin faktoriyelini hesaplayan programi  create ediniz.
+
+    //TASK 08 --> Istenilen bir sayinin  x. kuvvetini ekrana yazdiran programi  create ediniz.
+}
 
 
 
