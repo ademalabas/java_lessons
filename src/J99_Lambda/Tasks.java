ac56d6d997eb139f941f8a2ec3fe09dc8d1bbc4e
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-29 20:15:59 +0300

    29.11.22

---
+++ src/J99_Lambda/Tasks.java
@@ -0,0 +1,22 @@
+package J99_Lambda;
+
+public class Tasks {
+    public static void main(String[] args) {
+        //TASK 05 --> haluk.txt dosyasindaki farkli kelimeleri  print ediniz..
+        //TASK 06 --> haluk.txt dosyasindaki tum kelimeleri natural order  print ediniz..
+        //TASK 07 --> haluk.txt dosyasinda "basari" kelimesinin kac kere gectigini buyuk harf kucuk harf bagımsız print ediniz.
+        //TASK 08 --> haluk.txt dosyasinda "a" harfi gecen kelimelerin sayisini print eden  programi create ediniz.
+        //TASK 09 --> haluk.txt dosyasinda icinde "a" harfi gecen kelimeleri print ediniz.
+        //TASK 10 --> haluk.txt dosyasinda kac /farklı harf kullanildigini print ediniz.
+        //TASK 11 --> haluk.txt dosyasinda kac farkli kelime kullanildigini print ediniz.
+        //TASK 12 --> haluk.txt dosyasinda  farkli kelimeleri print ediniz
+
+
+
+
+
+
+    }
+
+
+}
